f606925454c0fa314efabbbacdf907cbfa925dc6
Flofi.dev <flofi.dev@gmail.com>
2022-12-28 00:49:14 +0000

    update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,11 +7,11 @@
 module.exports = {
   apps: [
     {
-      name: "prod", // pm2 start App name
+      name: "degens", // pm2 start App name
       script: "dist/src/server.js",
       exec_mode: "cluster", // 'cluster' or 'fork'
       instance_var: "INSTANCE_ID", // instance variable
-      instances: 2, // pm2 instance count
+      instances: 1, // pm2 instance count
       autorestart: true, // auto restart if process crash
       watch: false, // files change automatic restart
       ignore_watch: ["node_modules", "logs"], // ignore files change

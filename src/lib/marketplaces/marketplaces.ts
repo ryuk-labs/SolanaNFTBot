028f86a4fac02d146cafae33636e2c01aaab18b6
Florian Fiedler <f.fiedler@it-labs.de>
2023-01-03 22:09:28 +0100

    feat(app): remove magiceden

--- src/lib/marketplaces/marketplaces.ts
+++ src/lib/marketplaces/marketplaces.ts
@@ -1,17 +1,15 @@
-import { Marketplace } from "./types";
-import magicEden from "./magicEden";
-import digitalEyes from "./digitalEyes";
-import solanart from "./solanart";
 import alphaArt from "./alphaArt";
+import digitalEyes from "./digitalEyes";
 import exchangeArt from "./exchangeArt";
-import solsea from "./solsea";
 import openSea from "./openSea";
+import solanart from "./solanart";
+import solsea from "./solsea";
+import { Marketplace } from "./types";
 
 /**
  * These are the list of marketplaces that we check for notifications
  */
 const marketplaces: Marketplace[] = [
-  magicEden,
   digitalEyes,
   solanart,
   alphaArt,
